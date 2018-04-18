d9b937d6780343945d3a9aa33290841d3b4cca60
Sheng Li <lisheng2001@gmail.com>
2018-04-18 16:40:11 +1000

    Added custom view and view controller for news flash

---
+++ news app for blockchain/NewsFlashView.swift
@@ -0,0 +1,21 @@
+//
+//  NewsFlashView.swift
+//  news app for blockchain
+//
+//  Created by Sheng Li on 18/4/18.
+//  Copyright © 2018 Sheng Li. All rights reserved.
+//
+
+import UIKit
+
+class NewsFlashView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
