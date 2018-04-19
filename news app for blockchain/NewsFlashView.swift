1e2c486d34a4560947b3edd547ba49e54d4dfe5a
Sheng Li <lisheng2001@gmail.com>
2018-04-19 15:01:46 +1000

    Finished News Flash UI layout draft version

--- news app for blockchain/NewsFlashView.swift
+++ news app for blockchain/NewsFlashView.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 
+@IBDesignable
 class NewsFlashView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    @IBOutlet weak var newsFlashLabel: UILabel!
+    
     override func draw(_ rect: CGRect) {
         // Drawing code
     }
-    */
-
+    
 }
--- news app for blockchain/NewsFlashViewController.swift
+++ news app for blockchain/NewsFlashViewController.swift
@@ -9,9 +9,26 @@
 import UIKit
 
 class NewsFlashViewController: UIViewController {
-
+    
+    @IBOutlet weak var dateAndTimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let label = dateAndTimeLabel {
+            let formatter = DateFormatter()
+            //formatter.dateStyle = .medium
+            formatter.dateFormat = "M月d日 EEEE"
+            // formatter.timeStyle = .medium
+            formatter.locale = Locale(identifier: "zh_Hans")
+            label.text = "今天" + formatter.string(from: Date())
+            
+            label.layer.cornerRadius = label.frame.height / 4
+            // label.backgroundColor = .yellow
+            label.clipsToBounds = true
+            label.layer.borderWidth = 2
+            label.layer.borderColor = #colorLiteral(red: 0.7294117647, green: 0.7294117647, blue: 0.7294117647, alpha: 1)
+            
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -20,7 +37,6 @@ class NewsFlashViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
