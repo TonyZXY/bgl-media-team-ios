988faac166a38a564ded783340ef15835fc545c0
Sheng Li <lisheng2001@gmail.com>
2018-04-30 16:11:28 +1000

    Cleaned Timeline code and toggled colour of colour TimelinePoint.

--- news app for blockchain/Timeline/TimelineTableViewCell.swift
+++ news app for blockchain/Timeline/TimelineTableViewCell.swift
@@ -34,31 +34,13 @@ open class TimelineTableViewCell: UITableViewCell {
         }
     }
     
-    open var bubbleRadius: CGFloat = 2.0 {
-        didSet {
-            if (bubbleRadius < 0.0) {
-                bubbleRadius = 0.0
-            } else if (bubbleRadius > 6.0) {
-                bubbleRadius = 6.0
-            }
-            
-            self.setNeedsDisplay()
-        }
-    }
-    
     open var bubbleColor = UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0)
     
     override open func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
-    override open func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
         
-        // Configure the view for the selected state
-    }
-    
     override open func draw(_ rect: CGRect) {
         for layer in self.contentView.layer.sublayers! {
             if layer is CAShapeLayer {
@@ -70,11 +52,9 @@ open class TimelineTableViewCell: UITableViewCell {
         titleLabel.sizeToFit()
         descriptionLabel.sizeToFit()
         
-        // timelinePoint.position = CGPoint(x: timeline.leftMargin + timeline.width / 2, y: titleLabel.frame.origin.y + titleLabel.intrinsicContentSize.height / 2 - timelinePoint.diameter / 2)
         timelinePoint.position = CGPoint(x: timeline.leftMargin + timeline.width / 2, y: timelinePoint.lineWidth)
         
         timeline.start = CGPoint(x: timelinePoint.position.x + timelinePoint.diameter / 2, y: timelinePoint.position.y + timelinePoint.diameter)
-        timeline.middle = CGPoint(x: timeline.start.x, y: timelinePoint.position.y + timelinePoint.diameter)
         timeline.end = CGPoint(x: timeline.start.x, y: self.bounds.size.height)
         timeline.draw(view: self.contentView)
         
@@ -85,31 +65,5 @@ open class TimelineTableViewCell: UITableViewCell {
                                                    y: timelinePoint.position.y + (timelinePoint.diameter - timelinePointInside.diameter) / 2)
             timelinePointInside.draw(view: self.contentView)
         }
-        
-        if let title = titleLabel.text, !title.isEmpty {
-            drawBubble()
-        }
-    }
-    
-    fileprivate func drawBubble() {
-        let offset: CGFloat = 16
-        let bubbleRect = CGRect(
-            x: titleLabel.frame.origin.x - offset / 2,
-            y: titleLabel.frame.origin.y - offset / 2,
-            width: titleLabel.intrinsicContentSize.width + offset,
-            height: titleLabel.intrinsicContentSize.height + offset)
-        
-        let path = UIBezierPath(roundedRect: bubbleRect, cornerRadius: bubbleRadius)
-        let startPoint = CGPoint(x: bubbleRect.origin.x, y: bubbleRect.origin.y + bubbleRect.height / 2 - 8)
-        path.move(to: startPoint)
-        path.addLine(to: startPoint)
-        path.addLine(to: CGPoint(x: bubbleRect.origin.x - 8, y: bubbleRect.origin.y + bubbleRect.height / 2))
-        path.addLine(to: CGPoint(x: bubbleRect.origin.x, y: bubbleRect.origin.y + bubbleRect.height / 2 + 8))
-        
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = path.cgPath
-        shapeLayer.fillColor = bubbleColor.cgColor
-        
-        self.contentView.layer.insertSublayer(shapeLayer, below: titleLabel.layer)
     }
 }
