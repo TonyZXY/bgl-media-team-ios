5134de388b04a6ed880804cd854dbcecb922853d
TonyZheng <xyzheng109@gmail.com>
2018-05-29 12:14:41 +1000

    change load image

--- news app for blockchain/News/Helpers/Extensions.swift
+++ news app for blockchain/News/Helpers/Extensions.swift
@@ -50,7 +50,8 @@ extension IndicatorView: Placeholder { /* Just leave it empty */}
 
 extension UIImageView {
     func setImage(urlString: String) {
-        self.kf.setImage(with: URL(string: urlString), placeholder: IndicatorView(frame: frame) as Placeholder)
+//        self.kf.setImage(with: URL(string: urlString), placeholder: IndicatorView(frame: frame) as Placeholder)
+        self.kf.setImage(with: URL(string: urlString), placeholder: UIImage(named: "loading"))
     }
 }
 
